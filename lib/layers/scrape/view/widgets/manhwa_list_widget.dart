98ad97cefe02041a397bfc2e42aa4417b4482d76
e-meyer <edmundo.meyer@deltasul.com.br>
2023-06-09 17:01:29 -0300

    feat: each manhwa website will have its row and it will be collapsed to see each manhwa details

--- lib/layers/scrape/view/widgets/manhwa_list_widget.dart
+++ lib/layers/scrape/view/widgets/manhwa_list_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ManhwaListWidget extends StatelessWidget {
+class ManhwaListWidget extends StatefulWidget {
   final List<Map<String, dynamic>> data;
 
   const ManhwaListWidget({
@@ -11,145 +11,212 @@ class ManhwaListWidget extends StatelessWidget {
   });
 
   @override
+  State<ManhwaListWidget> createState() => _ManhwaListWidgetState();
+}
+
+class _ManhwaListWidgetState extends State<ManhwaListWidget> {
+  bool _isExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
+    final website = widget.data[0]['website'];
+    final manhwaData = widget.data[0]['manhwa_data'];
+
     return SingleChildScrollView(
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: NeverScrollableScrollPhysics(),
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          final website = data[index]['website'];
-          final manhwaData = data[index]['manhwa_data'];
+      physics: NeverScrollableScrollPhysics(),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AnimatedCrossFade(
+            firstChild: ListView.separated(
+              shrinkWrap: true,
+              itemCount: widget.data.length,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                final website = widget.data[index]['website'];
+                final manhwaData = widget.data[index]['manhwa_data'];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 150,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        physics: BouncingScrollPhysics(),
+                        itemCount: 4,
+                        itemBuilder: (context, manhwaIndex) {
+                          final title = manhwaData[manhwaIndex]['title'];
+                          final coverLink =
+                              manhwaData[manhwaIndex]['cover_link'];
+                          final chapters = manhwaData[manhwaIndex]['chapters'];
+                          final chapterLinks =
+                              manhwaData[manhwaIndex]['chapters_links'];
+                          return Image.network(
+                            coverLink,
+                            fit: BoxFit.fitHeight,
+                            width: MediaQuery.of(context).size.width * 0.30,
+                            height: 150,
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: 20,
+                );
+              },
+            ),
+            secondChild: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 4,
+                  itemBuilder: (context, manhwaIndex) {
+                    final title = manhwaData[manhwaIndex]['title'];
+                    final coverLink = manhwaData[manhwaIndex]['cover_link'];
+                    final chapters = manhwaData[manhwaIndex]['chapters'];
+                    final chapterLinks =
+                        manhwaData[manhwaIndex]['chapters_links'];
 
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  website,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              ListView.separated(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: 4,
-                itemBuilder: (context, manhwaIndex) {
-                  final title = manhwaData[manhwaIndex]['title'];
-                  final coverLink = manhwaData[manhwaIndex]['cover_link'];
-                  final chapters = manhwaData[manhwaIndex]['chapters'];
-                  final chapterLinks =
-                      manhwaData[manhwaIndex]['chapters_links'];
-
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                    child: Row(
-                      children: [
-                        Image.network(
-                          coverLink,
-                          fit: BoxFit.fitHeight,
-                          width: MediaQuery.of(context).size.width * 0.30,
-                          height: 150,
-                        ),
-                        Expanded(
-                          child: Stack(
-                            children: [
-                              Center(
-                                child: Opacity(
-                                  opacity: 0.03,
-                                  child: Image.asset(
-                                    "assets/scanlators_logos/${website.toLowerCase()}.png",
-                                    height: 140,
-                                    // width
-                                    fit: BoxFit.fitHeight,
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 14.0),
+                      child: Row(
+                        children: [
+                          Image.network(
+                            coverLink,
+                            fit: BoxFit.fitHeight,
+                            width: MediaQuery.of(context).size.width * 0.30,
+                            height: 150,
+                          ),
+                          Expanded(
+                            child: Stack(
+                              children: [
+                                Center(
+                                  child: Opacity(
+                                    opacity: 0.03,
+                                    child: Image.asset(
+                                      "assets/scanlators_logos/${website.toLowerCase()}.png",
+                                      height: 140,
+                                      fit: BoxFit.fitHeight,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              // Center(
-                              //   child: Image.asset(
-                              //     "assets/scanlators_logos/asura.png",
-                              //     height: 140,
-                              //     // width
-                              //     fit: BoxFit.fitHeight,
-                              //   ),
-                              // ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 4.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(bottom: 12.0),
-                                      child: Text(
-                                        title,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: GoogleFonts.overpass(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 4.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 12.0),
+                                        child: Text(
+                                          title,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: GoogleFonts.overpass(
+                                            fontSize: 15,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: List.generate(chapters.length,
-                                          (chapterIndex) {
-                                        final chapter = chapters[chapterIndex];
-                                        final chapterLink =
-                                            chapterLinks[chapterIndex];
-                                        return InkWell(
-                                          onTap: () {
-                                            _launchUrl(Uri.parse(chapterLink));
-                                          },
-                                          child: ListTile(
-                                            dense: true,
-                                            contentPadding:
-                                                EdgeInsets.symmetric(
-                                              horizontal: 0.0,
-                                              vertical: 0.0,
-                                            ),
-                                            visualDensity: VisualDensity(
-                                              horizontal: 0,
-                                              vertical: -4,
-                                            ),
-                                            title: Text(
-                                              chapter,
-                                              style: GoogleFonts.overpass(
-                                                fontSize: 14,
-                                                color: Colors.white,
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: List.generate(chapters.length,
+                                            (chapterIndex) {
+                                          final chapter =
+                                              chapters[chapterIndex];
+                                          final chapterLink =
+                                              chapterLinks[chapterIndex];
+                                          return InkWell(
+                                            onTap: () {
+                                              _launchUrl(
+                                                  Uri.parse(chapterLink));
+                                            },
+                                            child: ListTile(
+                                              dense: true,
+                                              contentPadding:
+                                                  EdgeInsets.symmetric(
+                                                horizontal: 0.0,
+                                                vertical: 0.0,
+                                              ),
+                                              visualDensity: VisualDensity(
+                                                horizontal: 0,
+                                                vertical: -4,
+                                              ),
+                                              title: Text(
+                                                chapter,
+                                                style: GoogleFonts.overpass(
+                                                  fontSize: 14,
+                                                  color: Colors.white,
+                                                ),
                                               ),
                                             ),
-                                          ),
-                                        );
-                                      }),
-                                    ),
-                                  ],
+                                          );
+                                        }),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  return SizedBox(
-                    height: 20,
-                  );
-                },
-              ),
-            ],
-          );
-        },
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return SizedBox(
+                      height: 20,
+                    );
+                  },
+                ),
+              ],
+            ),
+            crossFadeState: _isExpanded
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            duration: Duration(milliseconds: 300),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _isExpanded = !_isExpanded;
+              });
+            },
+            child: Text(_isExpanded ? 'Collapse' : 'Expand'),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildRowOfCovers() {
+    return ListView.builder(
+        itemCount: widget.data.length,
+        itemBuilder: (context, index) {
+          // final website = value['website'];
+          final manhwaData = widget.data[index]['manhwa_data'];
+
+          // final title = manhwaData['title'];
+          final coverLink = manhwaData['cover_link'];
+
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 14.0),
+            child: Image.network(
+              coverLink,
+              fit: BoxFit.fitHeight,
+              width: MediaQuery.of(context).size.width * 0.30,
+              height: 150,
+            ),
+          );
+        });
   }
 
   Future<void> _launchUrl(url) async {
