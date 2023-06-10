8abec11b6c00a283bdb2ddded38aa7fd613d4d79
e-meyer <edmundomeyer01@gmail.com>
2023-06-10 20:09:35 -0300

    feat: add cloud_firestore dependency and fetched initial data in search_screen

--- lib/layers/search/view/screen/search_screen.dart
+++ lib/layers/search/view/screen/search_screen.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SearchScreen extends StatefulWidget {
+class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
-  State<SearchScreen> createState() => _SearchScreenState();
-}
-
-class _SearchScreenState extends State<SearchScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Color(0xFF222222),
-      body: Center(
-        child: Text('asdasdasd'),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('scanlators')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error retrieving data');
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                }
+
+                final scanlators = snapshot.data!.docs;
+
+                return ListView.builder(
+                  itemCount: scanlators.length,
+                  itemBuilder: (context, index) {
+                    final scanlator = scanlators[index];
+                    final scanlatorData =
+                        scanlator.data() as Map<String, dynamic>;
+
+                    return ListTile(
+                      leading: Image.network(scanlatorData['logo_url']),
+                      title: Text(scanlatorData['name']),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
