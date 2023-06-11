40c50352d3348c572dc09003f7cd906ad17d973a
e-meyer <edmundomeyer01@gmail.com>
2023-06-11 11:36:57 -0300

    docs: remove firebase_options from .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:manhwa_alert/helper_screen.dart';
-import 'package:manhwa_alert/layers/scrape/view/screen/scrape_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
