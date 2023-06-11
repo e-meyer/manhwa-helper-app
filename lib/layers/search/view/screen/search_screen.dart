5f2fef21daf4841b665085cf46bf1abaa90d3053
e-meyer <edmundomeyer01@gmail.com>
2023-06-11 17:14:35 -0300

    feat: search screen now displays scanlators cards for the user to select from which scanlator they want to search from

--- lib/layers/search/view/screen/search_screen.dart
+++ lib/layers/search/view/screen/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -8,47 +9,83 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF222222),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Flexible(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('scanlators')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error retrieving data');
-                }
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: [
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('scanlators')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error retrieving data');
+                  }
 
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
 
-                final scanlators = snapshot.data!.docs;
+                  var size = MediaQuery.of(context).size;
 
-                return GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  /*24 is for notification bar on Android*/
+                  final double itemHeight =
+                      (size.height - kToolbarHeight - 24) / 2;
+                  final double itemWidth = size.width / 2;
+                  final scanlators = snapshot.data!.docs;
+
+                  return GridView.count(
                     crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0,
-                  ),
-                  itemCount: scanlators.length,
-                  itemBuilder: (context, index) {
-                    final scanlator = scanlators[index];
-                    final scanlatorData =
-                        scanlator.data() as Map<String, dynamic>;
+                    crossAxisSpacing: 20.0,
+                    mainAxisSpacing: 20.0,
+                    shrinkWrap: true,
+                    childAspectRatio: (1 / 1.25),
+                    children: List.generate(scanlators.length, (index) {
+                      final scanlator = scanlators[index];
+                      final scanlatorData =
+                          scanlator.data() as Map<String, dynamic>;
 
-                    return ListTile(
-                      leading: Image.network(scanlatorData['logo_url']),
-                      title: Text(scanlatorData['name']),
-                    );
-                  },
-                );
-              },
-            ),
+                      return Container(
+                        height: 1000,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(int.parse(
+                              '0xFF${scanlatorData['card_bg_color']}')),
+                        ),
+                        child: Center(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  scanlatorData['logo_url'],
+                                  height: 150,
+                                  width: 150,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  scanlatorData['name'],
+                                  style: GoogleFonts.overpass(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
