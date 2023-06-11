97cdad54d18112998db84a9a668d022b818884a2
e-meyer <edmundomeyer01@gmail.com>
2023-06-11 18:02:05 -0300

    feat(helper_screen): improved navigation bar experience increasing the number of screens it shows up
    - Created function to handle the navigation for the body of HelperScreen, then screens outside of the '_screens' array will also be able to show the navigation bar at the bottom

--- lib/helper_screen.dart
+++ lib/helper_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:manhwa_alert/layers/scrape/view/screen/scrape_screen.dart';
+import 'package:manhwa_alert/layers/search/view/screen/scanlator_selection_screen.dart';
 import 'package:manhwa_alert/layers/search/view/screen/search_screen.dart';
 
 class HelperScreen extends StatefulWidget {
@@ -12,21 +13,31 @@ class HelperScreen extends StatefulWidget {
 class _HelperScreenState extends State<HelperScreen> {
   int _currentIndex = 0;
 
+  Map<int, GlobalKey<NavigatorState>> navigatorKeys = {
+    0: GlobalKey<NavigatorState>(),
+    1: GlobalKey<NavigatorState>(),
+    2: GlobalKey<NavigatorState>(),
+    3: GlobalKey<NavigatorState>(),
+  };
+
   List<Widget> _screens = [
     ScrapeScreen(),
-    SearchScreen(),
+    ScanlatorSelectionScreen(),
+    ScrapeScreen(),
+    ScrapeScreen(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_currentIndex],
+      body: buildNavigator(),
       bottomNavigationBar: CustomNavigationBar(
         scaleFactor: 0.01,
         strokeColor: Colors.transparent,
         backgroundColor: Color(0xFF262626),
         currentIndex: _currentIndex,
         onTap: (index) {
+          print(index);
           setState(() {
             _currentIndex = index;
           });
@@ -48,10 +59,46 @@ class _HelperScreenState extends State<HelperScreen> {
                   : ColorFilter.mode(Color(0xFF676767), BlendMode.srcIn),
             ),
           ),
-          // CustomNavigationBarItem(icon: Icon(Icons.notifications)),
-          // CustomNavigationBarItem(icon: Icon(Icons.settings)),
+          CustomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/notifications_bell.svg',
+              colorFilter: _currentIndex == 2
+                  ? ColorFilter.mode(Color(0xFFFFFFFF), BlendMode.srcIn)
+                  : ColorFilter.mode(Color(0xFF676767), BlendMode.srcIn),
+            ),
+          ),
+          CustomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/settings_engine.svg',
+              colorFilter: _currentIndex == 3
+                  ? ColorFilter.mode(Color(0xFFFFFFFF), BlendMode.srcIn)
+                  : ColorFilter.mode(Color(0xFF676767), BlendMode.srcIn),
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  buildNavigator() {
+    return Navigator(
+      key: navigatorKeys[_currentIndex],
+      onGenerateRoute: (RouteSettings settings) {
+        final args = settings.arguments;
+
+        if (settings.name == 'search') {
+          if (args is Map) {
+            return MaterialPageRoute(
+              builder: (_) => SearchScreen(
+                scanlatorName: args['name'],
+                scanlatorLogoUrl: args['logo_url'],
+              ),
+            );
+          }
+        }
+        return MaterialPageRoute(
+            builder: (_) => _screens.elementAt(_currentIndex));
+      },
     );
   }
 }
