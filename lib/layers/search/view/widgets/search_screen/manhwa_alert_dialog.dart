6b7b338afbc235a7ae0278673423e984c2b56d22
e-meyer <edmundomeyer01@gmail.com>
2023-06-14 16:21:07 -0300

    feat: bottomsheet and alertdialog to show manhwa details

---
+++ lib/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ManhwaAlertDialog extends StatelessWidget {
+  final Map<String, String> webtoon;
+
+  const ManhwaAlertDialog({
+    super.key,
+    required this.webtoon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      elevation: 0,
+      shadowColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.height * 0.65,
+        decoration: BoxDecoration(
+          color: Color(0xFF222222),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: Image.network(
+                  webtoon['cover']!,
+                  height: 400,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              color: Color(0xFF222222),
+              child: Text(
+                webtoon['title']!,
+                style: GoogleFonts.overpass(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/layers/search/view/widgets/search_screen/manhwa_modal_bottom_sheet.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ManhwaModalBottomSheet extends StatelessWidget {
+  final Map<String, String> webtoon;
+
+  const ManhwaModalBottomSheet({
+    super.key,
+    required this.webtoon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(20),
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage(webtoon['cover']!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  webtoon['title']!,
+                  style: GoogleFonts.overpass(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
