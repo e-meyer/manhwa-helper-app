6bf44b522b7169efbd6a2ec7618295e00002e04c
e-meyer <edmundomeyer01@gmail.com>
2023-06-14 16:20:16 -0300

    feat: add functions to show manhwa details using BottomSheet and AletDialog - to be decided

--- lib/layers/search/view/widgets/search_screen/manhwa_search_result_list_builder.dart
+++ lib/layers/search/view/widgets/search_screen/manhwa_search_result_list_builder.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:manhwa_alert/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart';
+import 'package:manhwa_alert/layers/search/view/widgets/search_screen/manhwa_modal_bottom_sheet.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:flutter/scheduler.dart';
 
@@ -55,76 +57,94 @@ class _ManhwaSearchResultListBuilderState
     });
   }
 
+  void _showManhwaBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (_) => ManhwaModalBottomSheet(webtoon: widget.webtoon),
+    );
+  }
+
+  void _showManhwaAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => ManhwaAlertDialog(webtoon: widget.webtoon),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.network(
-          widget.webtoon['cover']!,
-          width: 300,
-          height: 300,
-          fit: BoxFit.cover,
-          loadingBuilder: (BuildContext context, Widget child,
-              ImageChunkEvent? loadingProgress) {
-            if (loadingProgress == null) {
-              return child;
-            }
-            return Shimmer.fromColors(
-              baseColor: Color(0xFF292929),
-              highlightColor: Color(0xFF333333),
+    return GestureDetector(
+      onTap: () => _showManhwaAlertDialog(context), // Show modal on tap
+      child: Stack(
+        children: [
+          Image.network(
+            widget.webtoon['cover']!,
+            width: 300,
+            height: 300,
+            fit: BoxFit.cover,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return Shimmer.fromColors(
+                baseColor: Color(0xFF292929),
+                highlightColor: Color(0xFF333333),
+                child: Container(
+                  color: Color(0xFF292929),
+                ),
+              );
+            },
+          ),
+          Visibility(
+            visible: !isLoading,
+            child: Align(
+              alignment: Alignment.bottomCenter,
               child: Container(
-                color: Color(0xFF292929),
-              ),
-            );
-          },
-        ),
-        Visibility(
-          visible: !isLoading,
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              width: double.infinity,
-              height: 80,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    Colors.black.withOpacity(0.8),
-                    Colors.transparent,
-                  ],
+                width: double.infinity,
+                height: 80,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black.withOpacity(0.8),
+                      Colors.transparent,
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        // Visibility(
-        //   visible: !isLoading,
-        //   child:
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 2.0,
-              horizontal: 2.0,
-            ),
-            child: Text(
-              widget.webtoon['title']!,
-              style: GoogleFonts.overpass(
-                color: Colors.white,
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
+          // Visibility(
+          //   visible: !isLoading,
+          //   child:
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 2.0,
+                horizontal: 2.0,
               ),
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
+              child: Text(
+                widget.webtoon['title']!,
+                style: GoogleFonts.overpass(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                ),
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          // ),
-        )
-      ],
+            // ),
+          )
+        ],
+      ),
     );
   }
 }
