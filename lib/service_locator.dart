e917d4364f6caec4bec3d01750697edab176f974
e-meyer <edmundomeyer01@gmail.com>
2023-06-16 17:32:12 -0300

    feat: add notifications testing files with initial service_locator

---
+++ lib/service_locator.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'notification_service.dart';
+
+final GetIt serviceLocator = GetIt.instance;
+
+Future<void> setupLocator() async {
+  if (!serviceLocator.isRegistered<SharedPreferences>()) {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    serviceLocator.registerSingleton<SharedPreferences>(sharedPreferences);
+  }
+  if (!serviceLocator.isRegistered<NotificationService>()) {
+    serviceLocator.registerSingleton<NotificationService>(
+      NotificationService(
+        serviceLocator(),
+      ),
+    );
+  }
+}
