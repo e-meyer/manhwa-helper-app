e379d15ad95fd8b424c7bfff6f8e8e8e01b2d943
e-meyer <edmundomeyer01@gmail.com>
2023-06-16 10:45:13 -0300

    feat: helper_screen navigation bar now points to notifications screen

--- lib/helper_screen.dart
+++ lib/helper_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:manhwa_alert/layers/home/view/screen/home_screen.dart';
+import 'package:manhwa_alert/layers/notifications/view/screen/notifications_screen.dart';
 import 'package:manhwa_alert/layers/search/models/scanlator_model.dart';
 import 'package:manhwa_alert/layers/search/view/screen/scanlator_selection_screen.dart';
 import 'package:manhwa_alert/layers/search/view/screen/search_screen.dart';
@@ -24,7 +25,7 @@ class _HelperScreenState extends State<HelperScreen> {
   List<Widget> _screens = [
     HomeScreen(),
     ScanlatorSelectionScreen(),
-    HomeScreen(),
+    NotificationsScreen(),
     HomeScreen(),
   ];
 
