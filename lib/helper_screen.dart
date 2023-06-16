1e1154ddd9f4109646e9e82d7ca4c9a1c4efcfc2
e-meyer <edmundomeyer01@gmail.com>
2023-06-16 20:58:55 -0300

    feat(helper_screen): add notifications handlers and modified bottomnavigationbar to support notification badge

--- lib/helper_screen.dart
+++ lib/helper_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:custom_navigation_bar/custom_navigation_bar.dart';
 import 'package:flutter_svg/svg.dart';
@@ -6,13 +7,20 @@ import 'package:manhwa_alert/layers/notifications/view/screen/notifications_scre
 import 'package:manhwa_alert/layers/search/models/scanlator_model.dart';
 import 'package:manhwa_alert/layers/search/view/screen/scanlator_selection_screen.dart';
 import 'package:manhwa_alert/layers/search/view/screen/search_screen.dart';
+import 'package:manhwa_alert/service_locator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'notification_service.dart';
 
 class HelperScreen extends StatefulWidget {
   @override
   _HelperScreenState createState() => _HelperScreenState();
 }
 
-class _HelperScreenState extends State<HelperScreen> {
+class _HelperScreenState extends State<HelperScreen>
+    with WidgetsBindingObserver {
+  final FirebaseMessaging _fcm = FirebaseMessaging.instance;
+  final NotificationService service = serviceLocator.get<NotificationService>();
   int _currentIndex = 0;
 
   Map<int, GlobalKey<NavigatorState>> navigatorKeys = {
@@ -30,6 +38,57 @@ class _HelperScreenState extends State<HelperScreen> {
   ];
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      SharedPreferences sp = await SharedPreferences.getInstance();
+      await sp.reload();
+      print(sp.getInt('newNotificationCount').toString());
+      print("testeeee  " + service.newNotificationCount.toString());
+      service.loadNotificationCount().then((value) => setState(() {}));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsFlutterBinding.ensureInitialized();
+    service.loadNotificationCount().then((value) => setState(() {
+          print("service ${service.newNotificationCount}");
+        }));
+
+    _fcm.getToken().then((token) => print('FCM Token: $token'));
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received a message while in the foreground:');
+      print('Message data: ${message.data}');
+      setState(() {
+        service.incrementNotificationCount();
+      });
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('&*&*&*&**&*&*&**&*&*&!@&*!@&*!@&*!&@*!@&*&!*');
+      print('Received a message and opened the app:');
+      print('Message data: ${message.data}');
+    });
+
+    _fcm.getInitialMessage().then((RemoteMessage? message) {
+      if (message != null) {
+        print('Received a message while the app was terminated:');
+        print('Message data: ${message.data}');
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: buildNavigator(),
@@ -41,10 +100,12 @@ class _HelperScreenState extends State<HelperScreen> {
         backgroundColor: Color(0xFF262626),
         currentIndex: _currentIndex,
         onTap: (index) {
-          print(index);
           setState(() {
             _currentIndex = index;
           });
+          if (_currentIndex == 2) {
+            service.resetNotificationCount();
+          }
         },
         items: [
           CustomNavigationBarItem(
@@ -88,13 +149,30 @@ class _HelperScreenState extends State<HelperScreen> {
             ),
           ),
           CustomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/icons/notifications-bell.svg',
-              colorFilter: ColorFilter.mode(
-                Color(0xFF676767),
-                BlendMode.srcIn,
-              ),
-              height: 28,
+            icon: Stack(
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/notifications-bell.svg',
+                  colorFilter: ColorFilter.mode(
+                    Color(0xFF676767),
+                    BlendMode.srcIn,
+                  ),
+                  height: 28,
+                ),
+                if (service.newNotificationCount > 0)
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+              ],
             ),
             selectedIcon: SvgPicture.asset(
               'assets/icons/notifications-bell-solid.svg',
