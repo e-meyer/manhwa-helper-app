e68a9fca8b50ea62ac19052a9cce23a4df565a71
e-meyer <edmundomeyer01@gmail.com>
2023-06-19 17:35:13 -0300

    feat: save notification locally when recieved onBackground

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:manhwa_alert/core/injector/service_locator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+import 'notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,15 +35,9 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
   final value = sp.getInt('newNotificationCount') ?? 0;
   await sp.setInt('newNotificationCount', value + 1);
-  print("value: " + value.toString());
-  // await setupLocator();
-  // NotificationService service = serviceLocator.get<NotificationService>();
-  // SharedPreferences sp = serviceLocator.get<SharedPreferences>();
-
-  // service.loadNotificationCount();
-  // service.incrementNotificationCount();
-  // service.loadNotificationCount();
-  // print("Teste: " + service.newNotificationCount.toString());
+  await setupLocator();
+  NotificationService service = serviceLocator.get<NotificationService>();
+  await service.saveNotification(message.data);
 }
 
 class MyApp extends StatelessWidget {
