fac5919ca60d4acb6b20e2e8ff6e78086f382fb6
e-meyer <edmundomeyer01@gmail.com>
2023-06-21 17:05:23 -0300

    feat: create tutorial for sync button that is going to be implemented

--- lib/layers/notifications/view/screen/notifications_screen.dart
+++ lib/layers/notifications/view/screen/notifications_screen.dart
@@ -1,12 +1,13 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:manhwa_alert/core/injector/service_locator.dart';
-import 'package:manhwa_alert/layers/common/widgets/arc/two_rotating_arc.dart';
 import 'package:manhwa_alert/layers/notifications/models/notification_model.dart';
 import 'package:manhwa_alert/layers/notifications/view/widgets/notification_section_builder.dart';
 import 'package:manhwa_alert/notification_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
@@ -18,13 +19,22 @@ class NotificationsScreen extends StatefulWidget {
 class NotificationsScreenState extends State<NotificationsScreen>
     with WidgetsBindingObserver {
   final NotificationService service = serviceLocator.get<NotificationService>();
+  late TutorialCoachMark tutorialCoachMark;
+
+  GlobalKey keyButton = GlobalKey();
 
   @override
   void initState() {
+    createTutorial();
+    Future.delayed(Duration.zero, showTutorial);
     super.initState();
     WidgetsFlutterBinding.ensureInitialized();
     service.notifications.addListener(_updateNotifications);
     WidgetsBinding.instance.addObserver(this);
+  }
+
+  void showTutorial() {
+    tutorialCoachMark.show(context: context);
   }
 
   @override
@@ -35,6 +45,85 @@ class NotificationsScreenState extends State<NotificationsScreen>
 
   void _updateNotifications() {
     setState(() {});
+  }
+
+  List<TargetFocus> _createTargets() {
+    List<TargetFocus> targets = [];
+    targets.add(
+      TargetFocus(
+        shape: ShapeLightFocus.RRect,
+        radius: 16,
+        identify: "keyButton",
+        keyTarget: keyButton,
+        alignSkip: Alignment.bottomRight,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+            align: ContentAlign.bottom,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "Sync",
+                    style: GoogleFonts.overpass(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pulvinar tortor eget maximus iaculis.",
+                      style: GoogleFonts.overpass(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    return targets;
+  }
+
+  void createTutorial() {
+    tutorialCoachMark = TutorialCoachMark(
+      targets: _createTargets(),
+      colorShadow: Colors.black,
+      textStyleSkip: GoogleFonts.overpass(
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+        fontSize: 16,
+      ),
+      textSkip: "Next",
+      paddingFocus: 14,
+      opacityShadow: 0.8,
+      // imageFilter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+      onFinish: () {
+        print("finish");
+      },
+      onClickTarget: (target) {
+        print('onClickTarget: $target');
+      },
+      onClickTargetWithTapPosition: (target, tapDetails) {
+        print("target: $target");
+        print(
+            "clicked at position local: ${tapDetails.localPosition} - global: ${tapDetails.globalPosition}");
+      },
+      onClickOverlay: (target) {
+        print('onClickOverlay: $target');
+      },
+      onSkip: () {
+        print("skip");
+      },
+    );
   }
 
   @override
@@ -81,6 +170,7 @@ class NotificationsScreenState extends State<NotificationsScreen>
         ),
         actions: [
           ElevatedButton(
+            key: keyButton,
             onPressed: () async {
               service.clearAllNotifications();
             },
