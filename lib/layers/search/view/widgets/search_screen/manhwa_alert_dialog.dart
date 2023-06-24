10a436938fa3e89f281375a0bb25e4f5fe206460
e-meyer <edmundomeyer01@gmail.com>
2023-06-24 00:47:04 -0300

    feat: applying style to alert dialog

--- lib/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart
+++ lib/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:manhwa_alert/layers/notifications/controller/notification_service.dart';
 import 'package:manhwa_alert/core/injector/service_locator.dart';
@@ -109,39 +110,94 @@ class _ManhwaAlertDialogState extends State<ManhwaAlertDialog> {
                   ),
                 ),
               ),
-            ValueListenableBuilder(
-              valueListenable: service.subscribedTopics,
-              builder: (BuildContext context, dynamic value, Widget? child) {
-                bool isUserSubscribed = service.subscribedTopics.value.keys
-                    .contains(cleanTopic(widget.webtoon['title']!));
-                return StreamBuilder<bool>(
-                  stream: _buttonController.stream,
-                  initialData: true,
-                  builder:
-                      (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                    bool isButtonEnabled = snapshot.data!;
-                    return ElevatedButton(
-                      onPressed: isButtonEnabled
-                          ? () {
-                              if (isUserSubscribed) {
-                                _unsubscribeFromTopic(widget.webtoon['title']!);
-                              } else {
-                                _subscribeToTopic(widget.webtoon['title']!);
-                              }
-                            }
-                          : null,
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: ValueListenableBuilder(
+                      valueListenable: service.subscribedTopics,
+                      builder:
+                          (BuildContext context, dynamic value, Widget? child) {
+                        bool isUserSubscribed = service
+                            .subscribedTopics.value.keys
+                            .contains(cleanTopic(widget.webtoon['title']!));
+                        return StreamBuilder<bool>(
+                          stream: _buttonController.stream,
+                          initialData: true,
+                          builder: (BuildContext context,
+                              AsyncSnapshot<bool> snapshot) {
+                            bool isButtonEnabled = snapshot.data!;
+                            return InkWell(
+                              onTap: isButtonEnabled
+                                  ? () {
+                                      if (isUserSubscribed) {
+                                        _unsubscribeFromTopic(
+                                            widget.webtoon['title']!);
+                                      } else {
+                                        _subscribeToTopic(
+                                            widget.webtoon['title']!);
+                                      }
+                                    }
+                                  : null,
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 3.0),
+                                    child: SvgPicture.asset(
+                                      'assets/icons/notifications-bell.svg',
+                                      height: 18,
+                                      colorFilter: ColorFilter.mode(
+                                        isUserSubscribed
+                                            ? Color(0xFF646464)
+                                            : Color(0xFFFF6812),
+                                        BlendMode.srcIn,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(
+                                    isUserSubscribed
+                                        ? 'Unsubscribe'
+                                        : 'Subscribe',
+                                    style: GoogleFonts.overpass(
+                                      color: isUserSubscribed
+                                          ? Color(0xFF646464)
+                                          : Color(0xFFFF6812),
+                                      fontSize: 16,
+                                      fontWeight: isUserSubscribed
+                                          ? FontWeight.w500
+                                          : FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  Flexible(
+                    child: InkWell(
+                      onTap: () {
+                        // Your onPressed logic for the new button
+                      },
                       child: Text(
-                        isUserSubscribed ? 'Unsubscribe' : 'Subscribe',
-                        style: GoogleFonts.overpass(
-                          color: Colors.white,
-                          fontSize: 20,
+                        'See more',
+                        style: TextStyle(
+                          color: Color(0xFFFF6812),
+                          fontSize: 16,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    );
-                  },
-                );
-              },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
