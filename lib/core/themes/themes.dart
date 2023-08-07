2726d6b79ec769ea4c3e483aab182d039afd61a9
e-meyer <edmundomeyer01@gmail.com>
2023-08-07 16:37:31 -0300

    feat: create lgiht theme with rosy theme

--- lib/core/themes/themes.dart
+++ lib/core/themes/themes.dart
@@ -41,12 +41,42 @@ final ThemeData mainDarkTheme = ThemeData(
 
 final ThemeData mainLightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: Color(0xFFFFFFFF),
-  scaffoldBackgroundColor: Color(0xFF151515),
+  primaryColor: const Color(0xFF590D22),
+  scaffoldBackgroundColor: const Color(0xFFFFF0F3),
+  cardColor: const Color(0xFFFFE5EB),
+  indicatorColor: const Color(0xFF590D22),
+  dividerColor: const Color(0xFF484848),
   fontFamily: 'Poppins',
-  textTheme: TextTheme(
-    bodyText1: TextStyle(color: Color(0xFF151515)),
-    bodyText2: TextStyle(color: Color(0xFF151515)),
-    // Add other TextStyle properties as needed
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 24,
+      color: Color(0xFF590D22),
+      fontWeight: FontWeight.bold,
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 18,
+      color: Color(0xFF590D22),
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: TextStyle(
+      fontSize: 16,
+      color: Color(0xFF590D22),
+      fontWeight: FontWeight.w600,
+    ),
+    // subtitle1: const TextStyle(
+    //   color: Color(0xFFBEBEBE),
+    //   fontWeight: FontWeight.normal,
+    // ),
+    // subtitle2: const TextStyle(
+    //   color: Color(0xFF8A8A8A),
+    //   fontWeight: FontWeight.w600,
+    // ),
+    labelMedium: TextStyle(
+      color: Color(0xFFFFB3C1),
+    ),
+    labelSmall:  TextStyle(
+      color: Color(0xFFFF758F),
+      fontWeight: FontWeight.w600,
+    ),
   ),
 );
