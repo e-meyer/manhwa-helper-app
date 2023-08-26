6bc89b32d66aa8551da2d9da109e12d50654ac2a
e-meyer <edmundomeyer01@gmail.com>
2023-08-26 11:50:54 -0300

    refactor: finding out what package is crashing the app - try google fonts

--- lib/layers/details/view/screen/details_screen.dart
+++ lib/layers/details/view/screen/details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class DetailsScreen extends StatefulWidget {
   final Map<String, dynamic> webtoon;
--- lib/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart
+++ lib/layers/search/view/widgets/search_screen/manhwa_alert_dialog.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:manhwa_alert/layers/notifications/controller/notification_service.dart';
 import 'package:manhwa_alert/core/injector/service_locator.dart';
 import 'package:manhwa_alert/layers/search/models/scanlator_model.dart';
